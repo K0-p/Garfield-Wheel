9ba4e57a8efe68e616b9e1593e44b1469c85b385
Gabriel <gabrielalexandarmoore@gmail.com>
2024-04-12 01:32:53 -0500

    integrating usb to main

---
+++ sw/fx/tx.c
@@ -0,0 +1,9 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "./usb/examples/boards/ek-tm4c123gxl/garf_wheel/usb_dev_gamepad.h"
+
+int main(void){
+	ucall();
+	return 1;
+}
+
---
+++ sw/src/examples/boards/ek-tm4c123gxl/garf_wheel/ttest.c
@@ -0,0 +1,6 @@
+#include "usb_dev_gamepad.h"
+
+int tetes(void){
+	//ucall();
+	return 0;
+}
